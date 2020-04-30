ac604309b0ece08539356238d05223001c404885
raccoon91 <qsx314@gmail.com>
2020-04-30 16:12:43 +0900

    add components

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet, purus vitae eleifend tristique, lorem
+        magna volutpat orci, et vehicula erat erat nec elit. Aenean posuere nunc ac cursus facilisis. Aenean vel porta
+        turpis, ut iaculis justo.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
