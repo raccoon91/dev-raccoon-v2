33e9614825b79f41b044c9d60499953bd25d3282
raccoon91 <qsx314@gmail.com>
2020-05-03 17:26:37 +0900

    add svg animation inspiration from squarelab

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,47 +1,71 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import styled from "styled-components";
+import { motion, useViewportScroll } from "framer-motion";
 
-const HomeContainer = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-
-  &.change-enter {
-    opacity: 0;
-    transform: translateY(30px);
-    color: red;
-  }
-
-  &.change-enter-active {
-    opacity: 1;
-    transform: translateY(0);
-    transition: opacity 0.5s, transform 0.5s;
-    color: blue;
-  }
-
-  &.change-exit {
-    opacity: 1;
-    transform: translateY(0);
-    color: green;
-  }
-
-  &.change-exit-active {
-    opacity: 0;
-    transform: translateY(30px);
-    transition: opacity 0.3s, transform 0.5s;
-    color: yellow;
-  }
+const HomeContainer = styled(motion.div)`
+  position: relative;
+  width: 100%;
+  height: 400vh;
+  background: linear-gradient(180deg, #48c6ef 0%, #6f86d6 100%);
 `;
 
+const SVG = styled.svg`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-top: -365px;
+  margin-left: -700px;
+`;
+
+const pageVariants = {
+  initial: { opacity: 0, y: 50 },
+  enter: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -50 },
+};
+
+const pageTransition = {
+  duration: 0.3,
+};
+
 const Home: FC = () => {
+  const [angle, setAngle] = useState<string>(
+    "983.4,101.6 983.4,668.4 416.6,668.4 416.6,101.89999999999998 416.6,101.89999999999998 416.6,101.9",
+  );
+  const { scrollYProgress } = useViewportScroll();
+
+  useEffect(() => {
+    scrollYProgress.onChange((value) => {
+      if (value <= 0.25) {
+        setAngle("983.4,101.6 983.4,668.4 416.6,668.4 416.6,101.89999999999998 416.6,101.89999999999998 416.6,101.9");
+      } else if (value > 0.25 && value <= 0.5) {
+        setAngle("890.9,54.3 1081.8,385 890.9,715.7 509.1,715.7 318.2,385 509.1,54.299999999999955");
+      } else if (value > 0.5 && value <= 0.75) {
+        setAngle("700,84.4 1047.1,685.6 352.9,685.6 352.9,685.6 352.9,685.6 352.9,685.6");
+      } else {
+        setAngle("983.4,101.6 983.4,668.4 416.6,668.4 416.6,101.89999999999998 416.6,101.89999999999998 416.6,101.9");
+      }
+    });
+
+    return () => scrollYProgress.destroy();
+  }, [scrollYProgress]);
+
   return (
-    <HomeContainer>
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet, purus vitae eleifend tristique, lorem
-        magna volutpat orci, et vehicula erat erat nec elit. Aenean posuere nunc ac cursus facilisis. Aenean vel porta
-        turpis, ut iaculis justo.
-      </p>
+    <HomeContainer initial="initial" animate="enter" exit="exit" variants={pageVariants} transition={pageTransition}>
+      <SVG width="1400" height="770">
+        <motion.polygon
+          initial={{
+            points: "983.4,101.6 983.4,668.4 416.6,668.4 416.6,101.89999999999998 416.6,101.89999999999998 416.6,101.9",
+            fill: "none",
+            stroke: "white",
+            strokeWidth: 10,
+          }}
+          animate={{ points: angle }}
+          transition={{
+            duration: 0.5,
+            ease: "easeInOut",
+          }}
+        />
+      </SVG>
     </HomeContainer>
   );
 };
