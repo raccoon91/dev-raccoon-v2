0c2eb1240f32ac69aae6718e6d5fc3cd722a0caa
raccoon91 <qsx314@gmail.com>
2020-05-04 19:24:23 +0900

    add Modal message

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,6 +2,9 @@ import React, { FC } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
+import { ReactComponent as Close } from "images/close.svg";
+import { ReactComponent as Tools } from "images/tools.svg";
+
 const ModalContainer = styled.div`
   position: fixed;
   top: 0;
@@ -22,13 +25,47 @@ const ModalOverlay = styled(motion.div)`
   cursor: pointer;
 `;
 
-const ModalWrapper = styled(motion.div)`
+const ModalFloat = styled(motion.div)`
   position: relative;
   z-index: 3;
 `;
 
-const ModalContent = styled(motion.div)`
+const ModalExpand = styled(motion.div)`
+  overflow: hidden;
   background-color: white;
+`;
+
+const ModalHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  height: 120px;
+  padding: 40px;
+`;
+
+const CloseIcon = styled(Close)`
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 680px;
+`;
+
+interface ITextProps {
+  size?: string;
+  weight?: string;
+  marginTop?: string;
+}
+const Text = styled.p<ITextProps>`
+  font-size: ${({ size }): string => size || "16px"};
+  font-weight: ${({ weight }): string => weight || "400"};
+  margin-top: ${({ marginTop }): string => marginTop || "0"};
 `;
 
 const overlayVariants = {
@@ -51,7 +88,7 @@ const overlayVariants = {
   },
 };
 
-const wrapperVariants = {
+const floatVariants = {
   initial: { y: "50vh" },
   animate: {
     y: "0",
@@ -71,7 +108,7 @@ const wrapperVariants = {
   },
 };
 
-const contentVariants = {
+const expandVariants = {
   initial: {
     width: "100px",
     height: "100px",
@@ -99,6 +136,24 @@ const contentVariants = {
   },
 };
 
+// const contentVariants = {
+//   initial: {
+//     opacity: 0,
+//   },
+//   animate: {
+//     opacity: 1,
+//     transition: {
+//       delay: 0.4,
+//     },
+//   },
+//   exit: {
+//     opacity: 0,
+//     transition: {
+//       delay: 0,
+//     },
+//   },
+// };
+
 interface IModalProps {
   handleCloseModal: () => void;
 }
@@ -112,9 +167,19 @@ const Modal: FC<IModalProps> = ({ handleCloseModal }) => {
         variants={overlayVariants}
         onClick={handleCloseModal}
       />
-      <ModalWrapper initial="initial" animate="animate" exit="exit" variants={wrapperVariants}>
-        <ModalContent initial="initial" animate="animate" exit="exit" variants={contentVariants}></ModalContent>
-      </ModalWrapper>
+      <ModalFloat initial="initial" animate="animate" exit="exit" variants={floatVariants}>
+        <ModalExpand initial="initial" animate="animate" exit="exit" variants={expandVariants}>
+          <ModalHeader>
+            <CloseIcon onClick={handleCloseModal} />
+          </ModalHeader>
+          <ModalContent>
+            <Tools width="200" height="200" />
+            <Text size="20px" weight="700" marginTop="64px">
+              준비 중입니다.
+            </Text>
+          </ModalContent>
+        </ModalExpand>
+      </ModalFloat>
     </ModalContainer>
   );
 };
